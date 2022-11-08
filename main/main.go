55b49b79c1314cbebfb082efa278d2f8d2bef8d1
Lien Chen <screen.leon@gmail.com>
2022-11-09 00:01:41 +0800

    Add log when request server

--- main/main.go
+++ main/main.go
@@ -19,5 +19,12 @@ func main() {
 	router.GET("/health", controllers.Health)
 
 	log.Println("listen on", port)
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	log.Fatal(http.ListenAndServe(":"+port, logRequest(router)))
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
